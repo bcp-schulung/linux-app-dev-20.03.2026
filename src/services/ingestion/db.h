3d3d3a4e84016a09af9422dd01195c693582d74f
butjo <johannes.but@gmail.com>
2026-03-17 07:30:53 +0000

    Delete ingestion draft

--- src/services/ingestion/db.h
+++
@@ -1,5 +0,0 @@
-#include <sqlite3.h>
-int open_db(sqlite3 *db);
-int close_db(sqlite3 *db);
-int setup_db(sqlite3 *db);
-int write_record(sqlite3 *db, const char* timestamp, const char* station,float temperature);
